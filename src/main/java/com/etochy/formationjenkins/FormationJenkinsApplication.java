ecf7e3301f7607ff058b2eeba9da2923d71bcc09
etochy <etochy@gmail.com>
2022-11-03 16:39:03 +0100

    projet spring

---
+++ src/main/java/com/etochy/formationjenkins/FormationJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.etochy.formationjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationJenkinsApplication.class, args);
+	}
+
+}
