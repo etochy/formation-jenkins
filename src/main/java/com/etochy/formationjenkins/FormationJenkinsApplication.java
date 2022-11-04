cf718380ab48912f6fc821826d8aaee249d401af
etochy <etochy@gmail.com>
2022-11-04 09:27:58 +0100

    ignore

--- src/main/java/com/etochy/formationjenkins/FormationJenkinsApplication.java
+++ src/main/java/com/etochy/formationjenkins/FormationJenkinsApplication.java
@@ -1,5 +1,7 @@
 package com.etochy.formationjenkins;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FormationJenkinsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FormationJenkinsApplication.class, args);
+		SpringApplication app = new SpringApplication(FormationJenkinsApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", 8083));
+		app.run(args);
 	}
-
 }
